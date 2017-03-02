b7d828aa11fd2cdb9867749868a8c3db92a5a8f2
ctrlaltf2 <23644849+ctrlaltf2@users.noreply.github.com>
2017-03-02 18:06:48 -0500

    Added some problems

--- prob2.cpp
+++ prob2.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 
-int main() {
-	long long int back = 1, front = 2, temp = 0, i = 0;
-	long long signed int sum = 0;
-	while(back < 4000000) {
-//		std::cout << back << std::endl;
-		if(back % 2 == 0) {
-			sum += back;
-		}
-		temp = front;
-		front = back + front;
-		back = temp;
-	}
-std::cout << sum << std::endl;
-return 0;
+typedef long long unsigned int uint64;
+
+
+// 	0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987
+
+int sum = 0;
+
+int main(void) {
+  uint64 back = 1, front = 1, a;
+  for(int i = 3;i <= 5000;++i) {
+    a = front;
+    front = back + front;
+    back = a;
+    if(back > 4000000)
+      break;
+    if(back % 2 == 0)
+      sum += back;
+  }
+  std::cout << sum << std::endl;
 }

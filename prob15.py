5ec94dbde3e170acbbe834153e1c3d760d6e5838
ctrlaltf2 <23644849+ctrlaltf2@users.noreply.github.com>
2021-11-02 00:00:49 -0400

    Rework!

--- prob15.py
+++
@@ -1,9 +0,0 @@
-# Only reason I'm using python is 3's ability to do calculations of large numbers such as (40!)
-from math import factorial
-
-gridsize = int(input("Enter a grid size: "))
-
-def ncr(n, r):
-    return factorial(n) / factorial(r) / factorial(n - r)
-
-print(ncr(gridsize * 2, gridsize))
---
+++ run.py
@@ -0,0 +1,29 @@
+import subprocess
+import os
+import sys
+
+folders = [
+    "0000-0099",
+    "0100-0199",
+    "0200-0299",
+    "0300-0399",
+    "0400-0499",
+    "0500-0599",
+    "0600-0699",
+    "0700-0799"
+]
+
+def run_problem(problem_num):
+    folder = folders[problem_num // 100]
+
+    rel_filepath = os.path.join(folder, f'p{problem_num:04}.cpp')
+    abs_binpath  = os.path.join('/tmp', f'p{problem_num:04}')
+
+    compile_command = ["g++", "-O3", "-march=native", "-mtune=native", rel_filepath, "-o", abs_binpath]
+    run_command = [abs_binpath]
+
+    subprocess.run(compile_command)
+    subprocess.run(abs_binpath)
+
+
+run_problem(int(sys.argv[1]))

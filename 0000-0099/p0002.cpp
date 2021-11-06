90611ce80c17838a1fb8aa30f25ee25a32d025c6
ctrlaltf2 <23644849+ctrlaltf2@users.noreply.github.com>
2021-11-06 11:49:06 -0400

    Problem 02: Use OEIS:A014445 formula

--- 0000-0099/p0002.cpp
+++ 0000-0099/p0002.cpp
@@ -2,15 +2,16 @@
 
 constexpr std::uint64_t limit{4'000'000};
 
-int main() { std::uint64_t sum{0};
-    std::uint64_t a{0}, b{1};
+int main() {
+    std::uint64_t sum{0};
+    std::uint64_t a{2}, b{0};
 
     while(b < limit) {
         if(b % 2 == 0)
             sum += b;
 
         std::swap(a, b);
-        b += a;
+        b = 4*a + b;
     }
 
     std::cout << sum << '\n';
